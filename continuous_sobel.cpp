b6404b2c51b87b841d3a4b779b14c6936d07c60b
Jacob Wildes <101469503+jacobcwildes@users.noreply.github.com>
2024-04-21 13:27:31 -0400

    Sobel works on local node, fails on cluster. OpenCV issue?

--- continuous_sobel.cpp
+++ continuous_sobel.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <mpi.h>
 #include <opencv2/opencv.hpp>
+#include <chrono>
 
 using namespace cv;
 using namespace std;
@@ -16,7 +17,7 @@ struct image_t {
 	int xsize;
 	int ysize;
 	int depth;	/* bytes */
-	unsigned char* pixels;
+	char* pixels;
 };
 
 struct convolve_data_t {
@@ -38,8 +39,7 @@ struct combine_data_t {
 
 
 /* very inefficient convolve code */
-static void* generic_convolve(void* argument) {
-
+void generic_convolve(struct convolve_data_t* data) {
 	int x, y, k, l, d;
 	uint32_t color;
 	int sum, depth, width;
@@ -47,11 +47,10 @@ static void* generic_convolve(void* argument) {
 	struct image_t* input;
 	struct image_t* output;
 	int* filter;
-	struct convolve_data_t* data;
+	
 	int ystart, yend;
 
 	/* Convert from void pointer to the actual data type */
-	data = (struct convolve_data_t*)argument;
 	input = data->input;
 	output = data->output;
 	filter = data->filter;
@@ -61,13 +60,12 @@ static void* generic_convolve(void* argument) {
 
 	depth = input->depth;
 	width = input->xsize * depth;
+	//printf("here");
 	//printf("start looping, start:%d, stop%d\n", ystart, yend);
 	//printf("ysize: %d\n", input->ysize);
 	/* handle border */
 	if (ystart == 0) ystart = 1;
 	if (yend == input->ysize) yend = input->ysize - 1;
-
-	printf("start looping, start:%d, stop%d\n", ystart, yend);
 
 	for (d = 0; d < depth; d++) {
 		for (x = 1; x < input->xsize - 1; x++) {
@@ -79,24 +77,19 @@ static void* generic_convolve(void* argument) {
 						sum += color * filter[(l + 1) * 3 + (k + 1)];
 					}
 				}
-
 				if (sum < 0) sum = 0;
 				if (sum > 255) sum = 255;
-
 				//Store the convolved date
 				if (ystart == 1) output->pixels[((y)*width) + x * depth + d] = sum;
 				else output->pixels[((y - ystart) * width) + x * depth + d] = sum;
-
 			}
 		}
 	}
 
-	return NULL;
 }
 
-static void combine(void* argument) {
+void combine(struct combine_data_t *data) {
 
-	struct combine_data_t* data;
 	struct image_t* sobelx;
 	struct image_t* sobely;
 	struct image_t* output;
@@ -108,7 +101,6 @@ static void combine(void* argument) {
 	int out;
 	int index;
 
-	data = (struct combine_data_t*)argument;
 	sobelx = data->sobelx;
 	sobely = data->sobely;
 	output = data->output;
@@ -121,7 +113,6 @@ static void combine(void* argument) {
 
 	if (ystart == 0) ystart = 1;
 	if (yend == sobelx->ysize) yend = sobelx->ysize - 1;
-	printf("start combining, start:%d, stop%d\n", ystart, yend);
 
 	for (y = ystart; y < yend; y++) {
 		for (x = 1; x < sobelx->xsize - 1; x++) {
@@ -129,7 +120,7 @@ static void combine(void* argument) {
 				if (ystart == 1) index = ((y)*width) + x * depth + d;
 				else index = ((y - ystart) * width) + x * depth + d;
 
-				out = sqrt(
+				out = (int) sqrt(
 					(sobelx->pixels[index] * sobelx->pixels[index]) +
 					(sobely->pixels[index] * sobely->pixels[index])
 				);
@@ -148,177 +139,173 @@ static void combine(void* argument) {
 
 int main()
 {
-	struct image_t image, storagex, storagey, storagec, sobel_x, sobel_y, new_image;
-	int result;
-	int num_ranks, rank;
-	int sizes[3];
-	unsigned char* pixels;
-	int size;
+	struct image_t *image, *storagex, *storagey, *storagec, *sobel_x, *sobel_y, *new_image;
+	struct convolve_data_t *convx, *convy;
+	struct combine_data_t *comb;
+	int world_size, my_rank;
 	int* offsets, * counts;
 	int first_flag = 1;
+	Mat frame_in;
+	int xsize = 320;
+	int ysize = 240;
+	int depth = 3;
+	int size = xsize * ysize * depth;
+	int result;
+	uint64_t start, finish;
 
-	int my_rank;
-	int world_size;
+	image = (image_t*)malloc(sizeof(image_t));
+	storagex = (image_t*)malloc(sizeof(image_t));
+	storagey = (image_t*)malloc(sizeof(image_t));
+	storagec = (image_t*)malloc(sizeof(image_t));
+	sobel_x = (image_t*)malloc(sizeof(image_t));
+	sobel_y = (image_t*)malloc(sizeof(image_t));
+	new_image = (image_t*)malloc(sizeof(image_t));
 
-	cv::VideoCapture capture(0);
+	convx = (convolve_data_t*)malloc(sizeof(convolve_data_t));
+	convy = (convolve_data_t*)malloc(sizeof(convolve_data_t));
 
-	if (!capture.isOpened()) {
-		cout << "No camera attatched :(" << endl;
+	comb = (combine_data_t*)malloc(sizeof(combine_data_t));
+
+	result = MPI_Init(NULL, NULL);
+	if (result != MPI_SUCCESS) {
+		fprintf(stderr, "Error starting MPI program!.\n");
+		MPI_Abort(MPI_COMM_WORLD, result);
 	}
 
-	namedWindow("Active Edge Detection");
-	resizeWindow("Image", 600, 600);
 
-	Mat frame;
-	unsigned char* image;
-
-	MPI_Init(NULL, NULL);
-
+	//Get number of tasks and our rank
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	cout << "Hello World from process " << my_rank << " out of " << world_size << " processes!!!" << endl;
-
-	//Take first frame and get necessary data
-	if (my_rank == 0) {
-		if (!capture.read(frame)) {
-			cout << "Couldn't get a frame" << endl;
-			return -1;
-		}
-		else {
-			image.pixels = frame.data;
-			image.ysize = frame.rows;
-			image.xsize = frame.cols;
-			image.depth = frame.channels();
-			sizes[0] = image.ysize;
-			sizes[1] = image.xsize;
-			sizes[2] = image.depth;
-		}
-	}
-	MPI_Bcast(sizes,
-		3,
-		MPI_INT,
-		0,
-		MPI_COMM_WORLD);
-	if (my_rank == 0) {
-		size = image.ysize * image.xsize * image.depth;
-		cout << "Size: " << size << endl;
-	}
-	else {
-		//allocate memory for the incoming pixels and store the dimensions from the previous broadcast
-		pixels = (unsigned char*)calloc(sizes[0] * sizes[1] * sizes[2], sizeof(unsigned char));
-		image.ysize = sizes[0];
-		image.xsize = sizes[1];
-		image.depth = sizes[2];
-		size = image.ysize * image.xsize * image.depth;
-		image.pixels = pixels;
-	}
-
+	
 	/* Allocate space for storage */
-	storagex.xsize = image.xsize;
-	storagex.ysize = image.ysize;
-	storagex.depth = image.depth;
-	storagex.pixels = calloc(image.xsize * image.ysize * image.depth, sizeof(char));
+	image->xsize = xsize;
+	image->ysize = ysize;
+	image->depth = depth;
+	image->pixels = (char*)calloc(size, sizeof(char));
 
-	storagey.xsize = image.xsize;
-	storagey.ysize = image.ysize;
-	storagey.depth = image.depth;
-	storagey.pixels = calloc(image.xsize * image.ysize * image.depth, sizeof(char));
+	storagex->xsize = xsize;
+	storagex->ysize = ysize;
+	storagex->depth = depth;
+	storagex->pixels = (char *)calloc(size, sizeof(char));
 
-	storagec.xsize = image.xsize;
-	storagec.ysize = image.ysize;
-	storagec.depth = image.depth;
-	storagec.pixels = calloc(image.xsize * image.ysize * image.depth, sizeof(char));
+	storagey->xsize = xsize;
+	storagey->ysize = ysize;
+	storagey->depth = depth;
+	storagey->pixels = (char *)calloc(size, sizeof(char));
+
+	storagec->xsize = xsize;
+	storagec->ysize = ysize;
+	storagec->depth = depth;
+	storagec->pixels = (char *)calloc(size, sizeof(char));
 
 	/* Allocate space for sobel_x output */
-	sobel_x.xsize = image.xsize;
-	sobel_x.ysize = image.ysize;
-	sobel_x.depth = image.depth;
-	sobel_x.pixels = calloc(image.xsize * image.ysize * image.depth, sizeof(char));
+	sobel_x->xsize = xsize;
+	sobel_x->ysize = ysize;
+	sobel_x->depth = depth;
+	sobel_x->pixels = (char *)calloc(size, sizeof(char));
 
 	/* Allocate space for sobel_y output */
-	sobel_y.xsize = image.xsize;
-	sobel_y.ysize = image.ysize;
-	sobel_y.depth = image.depth;
-	sobel_y.pixels = calloc(image.xsize * image.ysize * image.depth, sizeof(char));
+	sobel_y->xsize = xsize;
+	sobel_y->ysize = ysize;
+	sobel_y->depth = depth;
+	sobel_y->pixels = (char *)calloc(size, sizeof(char));
 
 	/* Allocate space for output image */
-	new_image.xsize = image.xsize;
-	new_image.ysize = image.ysize;
-	new_image.depth = image.depth;
-	new_image.pixels = calloc(image.xsize * image.ysize * image.depth, sizeof(char));
+	new_image->xsize = xsize;
+	new_image->ysize = ysize;
+	new_image->depth = depth;
+	new_image->pixels = (char *)calloc(size, sizeof(char));
 
 	//Allocate memory for the vectorized gather's offsets and counts
-	offsets = calloc(num_ranks, sizeof(int));
-	counts = calloc(num_ranks, sizeof(int));
+	offsets = (int *)calloc(world_size, sizeof(int));
+	counts = (int *)calloc(world_size, sizeof(int));
 
 	//Fill in the offsets as the ystarts and the counts as the chunk size
-	for (int i = 0; i < num_ranks; i++) {
-		offsets[i] = i * ((image.ysize / num_ranks) * image.xsize * image.depth);
-		counts[i] = ((image.ysize / num_ranks) * image.xsize * image.depth);
+	for (int i = 0; i < world_size; i++) {
+		offsets[i] = i * ((ysize / world_size) * xsize * depth);
+		counts[i] = ((ysize / world_size) * xsize * depth);
 	}
 	//Fill in the last bit of the image into the final rank
-	counts[num_ranks - 1] += ((image.ysize / num_ranks) * image.xsize * image.depth) % num_ranks;
+	counts[world_size - 1] += ((ysize / world_size) * xsize * depth) % world_size;
+	
 
-	while (1) {
+	VideoCapture capture("\\\\JACOB-PC\\e\\Cluster_MPI_Sobel\\x64\\Debug\\cluster_video.mp4");
 
-		if (my_rank == 0){
-			if (!capture.read(frame)) {
+
+
+	if (my_rank == 0 && !capture.isOpened()) {
+		cout << "No camera attatched :(" << endl;
+	}
+	cout << "Start" << endl;
+	if (my_rank == 0) start = std::chrono::system_clock::now().time_since_epoch().count();
+	for (int i = 0; i < 50; i++) {
+
+		cout << "start loop" << endl;
+
+		if (my_rank == 0) {
+
+			if (!capture.read(frame_in)) {
 				cout << "Couldn't get a frame" << endl;
 				return -1;
 			}
-			pixels = frame.data;
-			image.ysize = frame.rows;
-			image.xsize = frame.cols;
-			image.depth = frame.channels();
+			resize(frame_in, frame_in, Size(xsize, ysize));
+			memcpy(image->pixels, frame_in.data, size);
+			//imshow("Original", frame_in);
+
+			//waitKey(1);
+			
 		}
-		
-		
+		image->ysize = ysize;
+		image->xsize = xsize;
+		image->depth = depth;
+
+		cout << "start broadcast" << endl;
 		//broadcast the pixels to the other ranks
-		MPI_Bcast(image.pixels,
+		MPI_Bcast(image->pixels,
 			size,
 			MPI_CHAR,
 			0,
 			MPI_COMM_WORLD);
 
+
 		/* convolution */
 		//setup struct to send to the convolve routine
-		struct convolve_data_t convx;
-		convx.input = &image;
-		convx.output = &storagex;
-		convx.ystart = (image.ysize / num_ranks) * rank;
-		convx.yend = (image.ysize / num_ranks) * (rank + 1);
-		convx.filter = sobel_x_filter;
-		generic_convolve((void*)&convx);
+		
+		convx->input = image;
+		convx->output = storagex;
+		convx->ystart = (ysize / world_size) * my_rank;
+		convx->yend = (ysize / world_size) * (my_rank + 1);
+		convx->filter = sobel_x_filter;
+		generic_convolve(convx);
 
 		//All the data is in chunks in the seperate ranks
 		//Gather the data from the storage into sobel_x
-		MPI_Gatherv(storagex.pixels,
-			counts[rank],
+		MPI_Gatherv(storagex->pixels,
+			counts[my_rank],
 			MPI_CHAR,
-			sobel_x.pixels,
+			sobel_x->pixels,
 			counts,
 			offsets,
 			MPI_CHAR,
 			0,
 			MPI_COMM_WORLD);
-
+		
 		//setup struct to send to the convolve routine
-		struct convolve_data_t convy;
-		convy.input = &image;
-		convy.output = &storagey;
-		convy.ystart = (image.ysize / num_ranks) * rank;
-		convy.yend = (image.ysize / num_ranks) * (rank + 1);
-		convy.filter = sobel_y_filter;
-		generic_convolve((void*)&convy);
-
+		convy->input = image;
+		convy->output = storagey;
+		convy->ystart = (ysize / world_size) * my_rank;
+		convy->yend = (ysize / world_size) * (my_rank + 1);
+		convy->filter = sobel_y_filter;
+		generic_convolve(convy);
 
 		//All the data is in chunks in the seperate ranks
 		//Gather the data from the storage into sobel_y
-		MPI_Gatherv(storagey.pixels,
-			counts[rank],
+		MPI_Gatherv(storagey->pixels,
+			counts[my_rank],
 			MPI_CHAR,
-			sobel_y.pixels,
+			sobel_y->pixels,
 			counts,
 			offsets,
 			MPI_CHAR,
@@ -326,34 +313,44 @@ int main()
 			MPI_COMM_WORLD);
 
 		/* Combine to form output */
-		struct combine_data_t comb;
-		comb.sobelx = &storagex;
-		comb.sobely = &storagey;
-		comb.output = &storagec;
-		comb.ystart = (image.ysize / num_ranks) * rank;
-		comb.yend = (image.ysize / num_ranks) * (rank + 1);
-		comb.rank = rank;
-		combine((void*)&comb);
+		
+		comb->sobelx = storagex;
+		comb->sobely = storagey;
+		comb->output = storagec;
+		comb->ystart = (ysize / world_size) * my_rank;
+		comb->yend = (ysize / world_size) * (my_rank + 1);
+		comb->rank = my_rank;
+		combine(comb);
 
 		//All the data is in chunks in the seperate ranks
 		//Gather the data from the storagex into new image
-		MPI_Gatherv(storagec.pixels,
-			counts[rank],
+		MPI_Gatherv(storagec->pixels,
+			counts[my_rank],
 			MPI_CHAR,
-			new_image.pixels,
+			new_image->pixels,
 			counts,
 			offsets,
 			MPI_CHAR,
 			0,
 			MPI_COMM_WORLD);
 
-		frame.data = new_image.pixels;
+		cout << "start broadcast" << endl;
+		if (my_rank == 0) {
+			Mat frame_out(xsize, ysize, CV_8UC3, new_image->pixels);
+			resize(frame_out, frame_out, Size(640, 480));
+			//imshow("Active Edge Detection", frame_out);
 
-		imshow("Active Edge Detection", frame);
-
-		waitkey(0);
+			//waitKey(1);
+		}
+		
 	}
+	if (my_rank == 0) {
+		finish = std::chrono::system_clock::now().time_since_epoch().count();
 
+		cout << "Total Time: " << ((float)(finish - start))/10000000.0 << endl;
+		cout << "Frame Rate: " << 50/(((float)(finish - start)) / 10000000.0) << endl;
+	}
+	
 
 
 	MPI_Finalize();
